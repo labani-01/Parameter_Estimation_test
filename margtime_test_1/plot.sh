378f24bff1471f2d94c45aafeda388bccc47e129
Labani Roy <labannyo2000@gmail.com>
2024-05-13 17:54:16 -0400

    changes

--- margtime_test_1/plot.sh
+++
@@ -1,5 +0,0 @@
-pycbc_inference_plot_posterior --verbose\
-    --input-file demarg_inj.hdf \
-    --output-file demarg_inj.png \
-    --parameters 'primary_mass(mass1, mass2):mass1' 'secondary_mass(mass1, mass2):mass2' \
-    --z-arg snr --plot-injection-parameters
