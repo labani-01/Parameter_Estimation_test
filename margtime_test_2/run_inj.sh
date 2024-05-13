217930fa615ffd9f74c009b6e7abbe73dfa9abfd
Labani Roy <labannyo2000@gmail.com>
2024-05-13 17:54:16 -0400

    changes

--- margtime_test_2/run_inj.sh
+++
@@ -1,9 +0,0 @@
-pycbc_create_injections --verbose \
-        --config-files inj.ini \
-        --ninjections 1 \
-        --seed 10 \
-        --output-file inj.hdf \
-        --variable-params-section variable_params \
-        --static-params-section static_params \
-        --dist-section prior \
-        --force
