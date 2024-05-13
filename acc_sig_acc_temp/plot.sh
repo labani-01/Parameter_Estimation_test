ce593755e48315d15f4a989d7b7615c985b5a8db
Labani Roy <labannyo2000@gmail.com>
2024-05-13 17:52:08 -0400

    changes

---
+++ acc_sig_acc_temp/plot.sh
@@ -0,0 +1,20 @@
+pycbc_inference_plot_posterior --verbose\
+    --input-file demarg_inj.hdf \
+    --output-file demarg_inj_1.png \
+    --parameters \
+        'primary_mass(mass1, mass2):mass1' \
+        'secondary_mass(mass1, mass2):mass2' \
+        'primary_mass(mass1, mass2)/secondary_mass(mass1, mass2):q' \
+        '(mass1+mass2):M' acc \
+    --z-arg snr --plot-injection-parameters
+
+pycbc_inference_plot_posterior --verbose\
+    --input-file demarg_inj.hdf \
+    --output-file demarg_inj.png \
+    --parameters \
+        'primary_mass(mass1, mass2):mass1' \
+        'secondary_mass(mass1, mass2):mass2' \
+        'primary_mass(mass1, mass2)/secondary_mass(mass1, mass2):q' \
+        '(mass1+mass2):M' \
+        acc ra dec tc inclination coa_phase polarization distance \
+    --z-arg snr --plot-injection-parameters
---
+++ zero-acc_sig_zero-acc_temp/PE.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+  
+export OMP_NUM_THREADS=1
+
+/home/lroy02/miniconda3/envs/gravity/bin/pycbc_inference \
+        --config-file "$1"  \
+        --nprocesses 16 \
+        --processing-scheme cpu \
+        --output-file "$2"\
+        --seed 0 \
+        --force \
+        --verbose
