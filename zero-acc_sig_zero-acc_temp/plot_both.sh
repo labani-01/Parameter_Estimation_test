cf68d36faf9ae50d50a04901134b775e2f070328
Labani Roy <labannyo2000@gmail.com>
2024-05-16 16:10:46 -0400

    both plots

--- zero-acc_sig_zero-acc_temp/plot_both.sh
+++ zero-acc_sig_zero-acc_temp/plot_both.sh
@@ -1,17 +1,15 @@
 pycbc_inference_plot_posterior --verbose\
-    --input-file demarg_without_a.hdf \
-    --input-file demarg_inj.hdf \
+    --input-file demarg_inj.hdf demarg_without_a.hdf\
     --output-file demarg_both_intrinsic.png \
     --parameters \
         'primary_mass(mass1, mass2):mass1' \
         'secondary_mass(mass1, mass2):mass2' \
         'primary_mass(mass1, mass2)/secondary_mass(mass1, mass2):q' \
         '(mass1+mass2):M' \
-    --z-arg snr --plot-injection-parameters
+    --z-arg snr 
 
 pycbc_inference_plot_posterior --verbose\
-    --input-file demarg_without_a.hdf \
-    --input-file demarg_inj.hdf \
+    --input-file demarg_inj.hdf demarg_without_a.hdf\
     --output-file demarg_both_extrinsic.png \
     --parameters \
         'primary_mass(mass1, mass2):mass1' \
@@ -19,4 +17,4 @@ pycbc_inference_plot_posterior --verbose\
         'primary_mass(mass1, mass2)/secondary_mass(mass1, mass2):q' \
         '(mass1+mass2):M' \
         ra dec tc inclination coa_phase polarization distance \
-    --z-arg snr --plot-injection-parameters
+    --z-arg snr 
