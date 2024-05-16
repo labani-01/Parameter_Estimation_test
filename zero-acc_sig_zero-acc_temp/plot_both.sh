853be84a327ab96d8c2bbc37d2dc7c16c865e94a
Labani Roy <labannyo2000@gmail.com>
2024-05-16 16:06:52 -0400

    both_plots

---
+++ zero-acc_sig_zero-acc_temp/plot_both.sh
@@ -0,0 +1,22 @@
+pycbc_inference_plot_posterior --verbose\
+    --input-file demarg_without_a.hdf \
+    --input-file demarg_inj.hdf \
+    --output-file demarg_both_intrinsic.png \
+    --parameters \
+        'primary_mass(mass1, mass2):mass1' \
+        'secondary_mass(mass1, mass2):mass2' \
+        'primary_mass(mass1, mass2)/secondary_mass(mass1, mass2):q' \
+        '(mass1+mass2):M' \
+    --z-arg snr --plot-injection-parameters
+
+pycbc_inference_plot_posterior --verbose\
+    --input-file demarg_without_a.hdf \
+    --input-file demarg_inj.hdf \
+    --output-file demarg_both_extrinsic.png \
+    --parameters \
+        'primary_mass(mass1, mass2):mass1' \
+        'secondary_mass(mass1, mass2):mass2' \
+        'primary_mass(mass1, mass2)/secondary_mass(mass1, mass2):q' \
+        '(mass1+mass2):M' \
+        ra dec tc inclination coa_phase polarization distance \
+    --z-arg snr --plot-injection-parameters
