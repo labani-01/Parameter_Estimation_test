daddbcbc5fb66846ffa6f0546bb29398b5b86185
Labani Roy <labannyo2000@gmail.com>
2024-05-17 11:16:56 -0400

    changes

--- zero-acc_sig_zero-acc_temp/plot_both.sh
+++ zero-acc_sig_zero-acc_temp/plot_both.sh
@@ -1,20 +1,22 @@
-pycbc_inference_plot_posterior --verbose\
-    --input-file demarg_inj.hdf demarg_without_a.hdf\
-    --output-file demarg_both_intrinsic.png \
-    --parameters \
-        'primary_mass(mass1, mass2):mass1' \
-        'secondary_mass(mass1, mass2):mass2' \
-        'primary_mass(mass1, mass2)/secondary_mass(mass1, mass2):q' \
-        '(mass1+mass2):M' \
-    --z-arg snr 
-
-pycbc_inference_plot_posterior --verbose\
-    --input-file demarg_inj.hdf demarg_without_a.hdf\
-    --output-file demarg_both_extrinsic.png \
+pycbc_inference_extract_samples --verbose \
+    --input-file demarg_without_a.hdf \
+    --output-file GW150914.hdf \
     --parameters \
         'primary_mass(mass1, mass2):mass1' \
         'secondary_mass(mass1, mass2):mass2' \
         'primary_mass(mass1, mass2)/secondary_mass(mass1, mass2):q' \
         '(mass1+mass2):M' \
         ra dec tc inclination coa_phase polarization distance \
-    --z-arg snr 
+    --skip-groups data sampler_info
+
+
+pycbc_inference_extract_samples --verbose \
+    --input-file demarg_inj.hdf \
+    --output-file injection.hdf \
+    --parameters \
+        'primary_mass(mass1, mass2):mass1' \
+        'secondary_mass(mass1, mass2):mass2' \
+        'primary_mass(mass1, mass2)/secondary_mass(mass1, mass2):q' \
+        '(mass1+mass2):M' \
+        ra dec tc inclination coa_phase polarization distance \
+    --skip-groups data sampler_info
