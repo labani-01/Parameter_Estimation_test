90d8b28b3203528bb9f8777b7f3e3e0c67321d07
Labani Roy <labannyo2000@gmail.com>
2024-06-05 10:06:49 -0400

    plots

--- GW150914_output/plot_together.sh
+++ GW150914_output/plot_together.sh
@@ -1,26 +1,27 @@
 pycbc_inference_plot_posterior --verbose\
-    --input-file GW150914_095045-PYCBC-POSTERIOR-IMRPhenomXPHM.hdf \
-    --output-file A1_existing_GW150914_masses.png \
+    --input-file demarg_without_a.hdf \
+    --output-file A1_GW150914_masses.png \
     --parameters \
-        'primary_mass(srcmass1/(1+redshift), srcmass2/(1+redshift)):mass1' \
-        'secondary_mass(srcmass1/(1+redshift), srcmass2/(1+redshift)):mass2' \
-        'mchirp_from_mass1_mass2(primary_mass(srcmass1/(1+redshift), srcmass2/(1+redshift)), secondary_mass(srcmass1/(1+redshift), srcmass2/(1+redshift))):mchirp' \
-        'primary_mass(srcmass1/(1+redshift), srcmass2/(1+redshift))/secondary_mass(srcmass1/(1+redshift), srcmass2/(1+redshift)):q' \
+        'primary_mass(mass1, mass2):mass1' \
+        'secondary_mass(mass1, mass2):mass2' \
+        'mchirp_from_mass1_mass2(mass1, mass2):mchirp' \
+        'primary_mass(mass1, mass2)/secondary_mass(mass1, mass2):q' \
     --z-arg snr
 
 
 pycbc_inference_plot_posterior --verbose\
-    --input-file GW150914_095045-PYCBC-POSTERIOR-IMRPhenomXPHM.hdf \
-    --output-file A2_existing_GW150914_spins.png \
+    --input-file demarg_without_a.hdf \
+    --output-file A2_GW150914_spins.png \
     --parameters \
         spin1_a spin2_a spin1_azimuthal spin2_azimuthal spin1_polar spin2_polar \
-        chi_eff chi_p \        
+        'chi_eff_from_spherical(mass1, mass2, spin1_a, spin1_polar, spin2_a, spin2_polar):chi_eff' \
+        'chi_p_from_spherical(mass1, mass2, spin1_a, spin1_azimuthal, spin1_polar, spin2_a, spin2_azimuthal, spin2_polar):chi_p' \        
     --z-arg snr
 
 
 pycbc_inference_plot_posterior --verbose\
-    --input-file GW150914_095045-PYCBC-POSTERIOR-IMRPhenomXPHM.hdf \
-    --output-file A3_existing_GW150914_extrinsic.png \
+    --input-file demarg_without_a.hdf \
+    --output-file A3_GW150914_extrinsic.png \
     --parameters \
         ra dec \
         inclination distance \
