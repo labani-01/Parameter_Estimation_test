2d6da6bb332c955be5ae6af46b4503da217f48ca
Labani Roy <labannyo2000@gmail.com>
2024-06-20 19:27:54 -0400

    changes

--- GW150914_output/get_existing_hdf_files.sh
+++ GW150914_output/get_existing_hdf_files.sh
@@ -2,10 +2,10 @@ pycbc_inference_extract_samples --verbose\
     --input-file GW150914_095045-PYCBC-POSTERIOR-IMRPhenomXPHM.hdf \
     --output-file GW150914_095045-PYCBC-POSTERIOR-IMRPhenomXPHM_updated.hdf \
     --parameters \
-        'primary_mass(srcmass1/(1+redshift), srcmass2/(1+redshift)):mass1' \
-        'secondary_mass(srcmass1/(1+redshift), srcmass2/(1+redshift)):mass2' \
-        'mchirp_from_mass1_mass2(primary_mass(srcmass1/(1+redshift), srcmass2/(1+redshift)), secondary_mass(srcmass1/(1+redshift), srcmass2/(1+redshift))):mchirp' \
-        'primary_mass(srcmass1/(1+redshift), srcmass2/(1+redshift))/secondary_mass(srcmass1/(1+redshift), srcmass2/(1+redshift)):q' \
+        'primary_mass(srcmass1*(1+redshift), srcmass2*(1+redshift)):mass1' \
+        'secondary_mass(srcmass1*(1+redshift), srcmass2*(1+redshift)):mass2' \
+        'mchirp_from_mass1_mass2(primary_mass(srcmass1*(1+redshift), srcmass2*(1+redshift)), secondary_mass(srcmass1*(1+redshift), srcmass2*(1+redshift))):mchirp' \
+        'primary_mass(srcmass1*(1+redshift), srcmass2*(1+redshift))/secondary_mass(srcmass1*(1+redshift), srcmass2*(1+redshift)):q' \
         spin1_a spin2_a spin1_azimuthal spin2_azimuthal spin1_polar spin2_polar chi_eff chi_p \
         ra dec \
         inclination distance \
--- GW150914_output/plot_acc_GW150914.sh
+++ GW150914_output/plot_acc_GW150914.sh
@@ -6,7 +6,6 @@ pycbc_inference_plot_posterior --verbose\
         'secondary_mass(mass1, mass2):mass2' \
         'mchirp_from_mass1_mass2(mass1, mass2):mchirp' \
         'primary_mass(mass1, mass2)/secondary_mass(mass1, mass2):q' \
-        acc \
     --z-arg snr
 
 
