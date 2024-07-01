1535fcbc0acec4eba8f93d314c48491e1c452177
Labani Roy <labannyo2000@gmail.com>
2024-06-30 20:46:52 -0400

    add

---
+++ GW150914_output/plot_new.sh
@@ -0,0 +1,28 @@
+pycbc_inference_plot_posterior --verbose\
+    --input-file inference_modified.hdf \
+    --output-file inference_modified_masses.png \
+    --parameters \
+        'primary_mass(mass1, mass2):mass1' \
+        'secondary_mass(mass1, mass2):mass2' \
+        'mchirp_from_mass1_mass2(mass1, mass2):mchirp' \
+        'primary_mass(mass1, mass2)/secondary_mass(mass1, mass2):q' \
+    --z-arg snr
+
+
+pycbc_inference_plot_posterior --verbose\
+    --input-file inference_modified.hdf \
+    --output-file inference_modified_spins.png \
+    --parameters \
+        spin1_a spin2_a spin1_azimuthal spin2_azimuthal spin1_polar spin2_polar \
+        'chi_eff_from_spherical(mass1, mass2, spin1_a, spin1_polar, spin2_a, spin2_polar):chi_eff' \
+        'chi_p_from_spherical(mass1, mass2, spin1_a, spin1_azimuthal, spin1_polar, spin2_a, spin2_azimuthal, spin2_polar):chi_p' \
+    --z-arg snr
+
+
+pycbc_inference_plot_posterior --verbose\
+    --input-file inference_modified.hdf \
+    --output-file inference_modified_extrinsic.png \
+    --parameters \
+        ra dec tc inclination coa_phase polarization distance \
+    --z-arg snr 
+
