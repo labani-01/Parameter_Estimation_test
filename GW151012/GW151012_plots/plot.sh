9cf2d0cec412d7b02e93f1df56fad84ccf1072d0
Labani Roy <labannyo2000@gmail.com>
2024-10-04 11:04:33 -0400

    Added_GW151012_base-model_plots

---
+++ GW151012/GW151012_plots/plot.sh
@@ -0,0 +1,28 @@
+pycbc_inference_plot_posterior --verbose\
+    --input-file /home/lroy02/acceleration_project/GW151012/demarg_IMRPhenomXPHM_GW151012.hdf \
+    --output-file A1_IMRPhenomXPHM_GW151012_masses.png \
+    --parameters \
+        'primary_mass(mass1, mass2):mass1' \
+        'secondary_mass(mass1, mass2):mass2' \
+        'mchirp_from_mass1_mass2(mass1, mass2):mchirp' \
+        'primary_mass(mass1, mass2)/secondary_mass(mass1, mass2):q' \
+    --z-arg snr
+
+
+pycbc_inference_plot_posterior --verbose\
+    --input-file /home/lroy02/acceleration_project/GW151012/demarg_IMRPhenomXPHM_GW151012.hdf \
+    --output-file A2_IMRPhenomXPHM_GW151012_spins.png \
+    --parameters \
+        spin1_a spin2_a spin1_azimuthal spin2_azimuthal spin1_polar spin2_polar \
+        'chi_eff_from_spherical(mass1, mass2, spin1_a, spin1_polar, spin2_a, spin2_polar):chi_eff' \
+        'chi_p_from_spherical(mass1, mass2, spin1_a, spin1_azimuthal, spin1_polar, spin2_a, spin2_azimuthal, spin2_polar):chi_p' \
+    --z-arg snr
+
+
+pycbc_inference_plot_posterior --verbose\
+    --input-file /home/lroy02/acceleration_project/GW151012/demarg_IMRPhenomXPHM_GW151012.hdf \
+    --output-file A3_IMRPhenomXPHM_GW151012_extrinsic.png \
+    --parameters \
+        ra dec \
+        inclination distance \
+    --z-arg snr
