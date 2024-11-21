e670e2636040a75733e75ec1ae5a8d6ddb516916
Labani Roy <labannyo2000@gmail.com>
2024-11-21 08:55:55 -0500

    large_acc_GW150914 and nonzero_acc_GW190521

--- GW150914/GW150914_plots/plot1.sh
+++ GW150914/GW150914_plots/plot1.sh
@@ -2,11 +2,9 @@ pycbc_inference_plot_posterior --verbose\
     --input-file /home/lroy02/acceleration_project/output_without_a/demarg_nonzero_large_acc.hdf \
     --output-file A1_nonzero_large_a_GW150914_masses1.png \
     --parameters \
-        'primary_mass(mass1, mass2):mass1' \
-        'secondary_mass(mass1, mass2):mass2' \
-        'mchirp_from_mass1_mass2(mass1, mass2):mchirp' \
-        'primary_mass(mass1, mass2)/secondary_mass(mass1, mass2):q' \
-        acc \
+        'mass1_from_mchirp_q(mchirp, q):mass1' \
+        'mass2_from_mchirp_q(mchirp, q):mass2' \
+        mchirp q \
     --z-arg snr
 
 
@@ -15,8 +13,8 @@ pycbc_inference_plot_posterior --verbose\
     --output-file A2_nonzero_large_a_GW150914_spins1.png \
     --parameters \
         spin1_a spin2_a spin1_azimuthal spin2_azimuthal spin1_polar spin2_polar \
-        'chi_eff_from_spherical(mass1, mass2, spin1_a, spin1_polar, spin2_a, spin2_polar):chi_eff' \
-        'chi_p_from_spherical(mass1, mass2, spin1_a, spin1_azimuthal, spin1_polar, spin2_a, spin2_azimuthal, spin2_polar):chi_p' \
+        'chi_eff_from_spherical(mass1_from_mchirp_q(mchirp, q), mass2_from_mchirp_q(mchirp, q), spin1_a, spin1_polar, spin2_a, spin2_polar):chi_eff' \
+        'chi_p_from_spherical(mass1_from_mchirp_q(mchirp, q), mass2_from_mchirp_q(mchirp, q), spin1_a, spin1_azimuthal, spin1_polar, spin2_a, spin2_azimuthal, spin2_polar):chi_p' \
     --z-arg snr
 
 
@@ -25,5 +23,5 @@ pycbc_inference_plot_posterior --verbose\
     --output-file A3_nonzero_large_a_GW150914_extrinsic1.png \
     --parameters \
         ra dec \
-        inclination distance \
+        inclination distance polarization coa_phase \
     --z-arg snr
