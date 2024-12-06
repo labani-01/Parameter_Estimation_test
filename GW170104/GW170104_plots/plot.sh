b8778c1a7d67a9740c27c345c399787a20f1b04e
Labani Roy <labannyo2000@gmail.com>
2024-12-06 10:53:12 -0500

    nonzero_acc_GW170104_GW170809_GW170823_GW190521

--- GW170104/GW170104_plots/plot.sh
+++ GW170104/GW170104_plots/plot.sh
@@ -1,28 +1,28 @@
 pycbc_inference_plot_posterior --verbose\
-    --input-file /home/lroy02/acceleration_project/GW170104/demarg_zero_a_GW170104.hdf \
-    --output-file A1_zero_a_GW170104_masses.png \
+    --input-file /home/lroy02/acceleration_project/GW170104/demarg_nonzero_a_GW170104.hdf \
+    --output-file A1_nonzero_a_GW170104_masses.png \
     --parameters \
-        'primary_mass(mass1, mass2):mass1' \
-        'secondary_mass(mass1, mass2):mass2' \
-        'mchirp_from_mass1_mass2(primary_mass(mass1, mass2), secondary_mass(mass1, mass2)):mchirp' \
-        'primary_mass(mass1, mass2)/secondary_mass(mass1, mass2):q' \
+        'mass1_from_mchirp_q(mchirp, q):mass1' \
+        'mass2_from_mchirp_q(mchirp, q):mass2' \
+        mchirp q \
+        acc \
     --z-arg snr
 
 
 pycbc_inference_plot_posterior --verbose\
-    --input-file /home/lroy02/acceleration_project/GW170104/demarg_zero_a_GW170104.hdf \
-    --output-file A2_zero_a_GW170104_spins.png \
+    --input-file /home/lroy02/acceleration_project/GW170104/demarg_nonzero_a_GW170104.hdf \
+    --output-file A2_nonzero_a_GW170104_spins.png \
     --parameters \
         spin1_a spin2_a spin1_azimuthal spin2_azimuthal spin1_polar spin2_polar \
-        'chi_eff_from_spherical(primary_mass(mass1, mass2), secondary_mass(mass1, mass2), spin1_a, spin1_polar, spin2_a, spin2_polar):chi_eff' \
-        'chi_p_from_spherical(primary_mass(mass1, mass2), secondary_mass(mass1, mass2), spin1_a, spin1_azimuthal, spin1_polar, spin2_a, spin2_azimuthal, spin2_polar):chi_p' \
+        'chi_eff_from_spherical(mass1_from_mchirp_q(mchirp, q), mass2_from_mchirp_q(mchirp, q), spin1_a, spin1_polar, spin2_a, spin2_polar):chi_eff' \
+        'chi_p_from_spherical(mass1_from_mchirp_q(mchirp, q), mass2_from_mchirp_q(mchirp, q), spin1_a, spin1_azimuthal, spin1_polar, spin2_a, spin2_azimuthal, spin2_polar):chi_p' \
     --z-arg snr
 
 
 pycbc_inference_plot_posterior --verbose\
-    --input-file /home/lroy02/acceleration_project/GW170104/demarg_zero_a_GW170104.hdf \
-    --output-file A3_zero_a_GW170104_extrinsic.png \
+    --input-file /home/lroy02/acceleration_project/GW170104/demarg_nonzero_a_GW170104.hdf \
+    --output-file A3_nonzero_a_GW170104_extrinsic.png \
     --parameters \
         ra dec \
-        inclination distance \
+        inclination distance polarization coa_phase \
     --z-arg snr
