4666fac32188cb2bca97834715f356256f91e393
Labani Roy <labannyo2000@gmail.com>
2025-04-06 21:58:40 -0400

    added_plot_after_resolving_error

--- zero-acc_sig_zero-acc_temp/run_inj.sh
+++ zero-acc_sig_zero-acc_temp/run_inj.sh
@@ -1,8 +1,8 @@
 pycbc_create_injections --verbose \
-        --config-files inj.ini \
+        --config-files inj_new.ini \
         --ninjections 1 \
         --seed 10 \
-        --output-file inj.hdf \
+        --output-file inj_new.hdf \
         --variable-params-section variable_params \
         --static-params-section static_params \
         --dist-section prior \
