10abab16d77f70de03a782b979d4780a9489a32b
Labani Roy <labannyo2000@gmail.com>
2025-04-08 15:55:30 -0400

    gaussian_IMR

--- workflow/test_high_low_mass/plot.sh
+++ workflow/test_high_low_mass/plot.sh
@@ -1,6 +1,6 @@
 pycbc_inference_plot_posterior --verbose\
-    --input-file /home/lroy02/ground_based_acc/project/test_settings/inference_GW190521_gauss.hdf \
-    --output-file inference_GW190521_gauss.png \
+    --input-file /home/lroy02/ground_based_acc/project/test_settings/inference_GW190521_IMR_gauss.hdf \
+    --output-file inference_GW190521_IMR_gauss.png \
     --parameters \
         'mass1_from_mchirp_q(mchirp, q):mass1' \
         'mass2_from_mchirp_q(mchirp, q):mass2' \
