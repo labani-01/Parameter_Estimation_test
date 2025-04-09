36fbb8d2280740cca828dfc0348d303eb554ea8a
Labani Roy <labannyo2000@gmail.com>
2025-04-09 12:21:04 -0400

    plots

--- workflow/test_high_low_mass/plot.sh
+++ workflow/test_high_low_mass/plot.sh
@@ -1,5 +1,5 @@
 pycbc_inference_plot_posterior --verbose\
-    --input-file /home/lroy02/PE/workflow/test_high_low_mass/H1L1V1-DEMARGINALIZATION_DEMARG_INFERENCE-GW190521_030229-0-10.hdf \
+    --input-file /home/lroy02/ground_based_acc/project/workflow/test/workflow_outputs1/H1L1V1-DEMARGINALIZATION_DEMARG_INFERENCE-GW190521_030229-0-10.hdf \
     --output-file H1L1V1-DEMARGINALIZATION_DEMARG_INFERENCE-GW190521_030229-0-10.png \
     --parameters \
         'mass1_from_mchirp_q(mchirp, q):mass1' \
