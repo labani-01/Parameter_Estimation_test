d35b1f7158b233a552243ba46b875cccf56e09b5
Labani Roy <labannyo2000@gmail.com>
2025-04-13 00:01:55 -0400

    plots from separate runs

--- GW150914/GW150914_plots/plot.sh
+++ GW150914/GW150914_plots/plot.sh
@@ -1,29 +1,28 @@
 pycbc_inference_plot_posterior --verbose\
-    --input-file /home/lroy02/acceleration_project/output_without_a/demarg_nonzero_large_acc.hdf \
-    --output-file A1_nonzero_large_a_GW150914_masses1.png \
+    --input-file /home/lroy02/ground_based_acc/separate_runs/GW150914/demarg_nonzero_a_GW150914.hdf \
+    --output-file A1_separate_nonzero_a_GW150914_masses.png \
     --parameters \
-        'primary_mass(mass1, mass2):mass1' \
-        'secondary_mass(mass1, mass2):mass2' \
-        'mchirp_from_mass1_mass2(mass1, mass2):mchirp' \
-        'primary_mass(mass1, mass2)/secondary_mass(mass1, mass2):q' \
+        'mass1_from_mchirp_q(mchirp, q):mass1' \
+        'mass2_from_mchirp_q(mchirp, q):mass2' \
+        mchirp q \
         acc \
     --z-arg snr
 
 
 pycbc_inference_plot_posterior --verbose\
-    --input-file /home/lroy02/acceleration_project/output_without_a/demarg_nonzero_large_acc.hdf \
-    --output-file A2_nonzero_large_a_GW150914_spins1.png \
+    --input-file /home/lroy02/ground_based_acc/separate_runs/GW150914/demarg_nonzero_a_GW150914.hdf \
+    --output-file A2_separate_nonzero_a_GW150914_spins.png \
     --parameters \
         spin1_a spin2_a spin1_azimuthal spin2_azimuthal spin1_polar spin2_polar \
-        'chi_eff_from_spherical(mass1, mass2, spin1_a, spin1_polar, spin2_a, spin2_polar):chi_eff' \
-        'chi_p_from_spherical(mass1, mass2, spin1_a, spin1_azimuthal, spin1_polar, spin2_a, spin2_azimuthal, spin2_polar):chi_p' \
+        'chi_eff_from_spherical(mass1_from_mchirp_q(mchirp, q), mass2_from_mchirp_q(mchirp, q), spin1_a, spin1_polar, spin2_a, spin2_polar):chi_eff' \
+        'chi_p_from_spherical(mass1_from_mchirp_q(mchirp, q), mass2_from_mchirp_q(mchirp, q), spin1_a, spin1_azimuthal, spin1_polar, spin2_a, spin2_azimuthal, spin2_polar):chi_p' \
     --z-arg snr
 
 
 pycbc_inference_plot_posterior --verbose\
-    --input-file /home/lroy02/acceleration_project/output_without_a/demarg_nonzero_large_acc.hdf \
-    --output-file A3_nonzero_large_a_GW150914_extrinsic1.png \
+    --input-file /home/lroy02/ground_based_acc/separate_runs/GW150914/demarg_nonzero_a_GW150914.hdf \
+    --output-file A3_separate_nonzero_a_GW150914_extrinsic.png \
     --parameters \
         ra dec \
-        inclination distance \
+        inclination distance polarization coa_phase \
     --z-arg snr
