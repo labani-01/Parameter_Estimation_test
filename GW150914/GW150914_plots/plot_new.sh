b1b4e096f551f5b27dfce51f11cf37856bcf95de
Labani Roy <labannyo2000@gmail.com>
2025-04-24 21:37:34 -0400

    plot_GW150914

---
+++ GW150914/GW150914_plots/plot_new.sh
@@ -0,0 +1,22 @@
+pycbc_inference_plot_posterior --verbose\
+    --input-file /home/lroy02/ground_based_acc/separate_runs/GW150914/demarg_nonzero_a_GW150914.hdf \
+    --output-file A1_separate_nonzero_a_GW150914_masses.png \
+    --parameters \
+        'mass1_from_mchirp_q(mchirp, q):mass1' \
+        'mass2_from_mchirp_q(mchirp, q):mass2' \
+        mchirp q \
+        acc \
+    --z-arg snr
+
+
+pycbc_inference_plot_posterior --verbose\
+    --input-file /home/lroy02/ground_based_acc/separate_runs/GW150914/demarg_nonzero_a_GW150914.hdf \
+    --output-file A2_separate_nonzero_a_GW150914_spins.png \
+    --parameters \
+        spin1_a spin2_a spin1_azimuthal spin2_azimuthal spin1_polar spin2_polar \
+        'chi_eff_from_spherical(mass1_from_mchirp_q(mchirp, q), mass2_from_mchirp_q(mchirp, q), spin1_a, spin1_polar, spin2_a, spin2_polar):chi_eff' \
+        'chi_p_from_spherical(mass1_from_mchirp_q(mchirp, q), mass2_from_mchirp_q(mchirp, q), spin1_a, spin1_azimuthal, spin1_polar, spin2_a, spin2_azimuthal, spin2_polar):chi_p' \
+        ra dec inclination distance
+    --z-arg snr
+
+
