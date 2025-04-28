7c25f67c72df9a89c136252f7021b1b5f6a5039c
Labani Roy <labannyo2000@gmail.com>
2025-04-27 21:45:15 -0400

    workflow plots check

--- GW170729/GW170729_plots/plot.sh
+++ GW170729/GW170729_plots/plot.sh
@@ -1,6 +1,6 @@
 pycbc_inference_plot_posterior --verbose\
-    --input-file /home/lroy02/ground_based_acc/separate_runs/GW170729/demarg_nonzero_a_GW170729.hdf \
-    --output-file A1_separate_nonzero_a_GW170729_masses.png \
+    --input-file /home/lroy02/ground_based_acc/project/workflow/test/workflow_outputs/local-site-scratch/work/H1L1V1-DEMARGINALIZATION_DEMARG_INFERENCE-GW170729_185629-0-10.hdf \
+    --output-file H1L1V1-PLOT_INFERENCE-GW170729_185629-0-10_masses.png \
     --parameters \
         'mass1_from_mchirp_q(mchirp, q):mass1' \
         'mass2_from_mchirp_q(mchirp, q):mass2' \
@@ -10,8 +10,8 @@ pycbc_inference_plot_posterior --verbose\
 
 
 pycbc_inference_plot_posterior --verbose\
-    --input-file  /home/lroy02/ground_based_acc/separate_runs/GW170729/demarg_nonzero_a_GW170729.hdf \
-    --output-file A2_separate_nonzero_a_GW170729_spins.png \
+    --input-file /home/lroy02/ground_based_acc/project/workflow/test/workflow_outputs/local-site-scratch/work/H1L1V1-DEMARGINALIZATION_DEMARG_INFERENCE-GW170729_185629-0-10.hdf \
+    --output-file H1L1V1-PLOT_INFERENCE-GW170729_185629-0-10_spins.png \
     --parameters \
         spin1_a spin2_a spin1_azimuthal spin2_azimuthal spin1_polar spin2_polar \
         'chi_eff_from_spherical(mass1_from_mchirp_q(mchirp, q), mass2_from_mchirp_q(mchirp, q), spin1_a, spin1_polar, spin2_a, spin2_polar):chi_eff' \
@@ -20,8 +20,8 @@ pycbc_inference_plot_posterior --verbose\
 
 
 pycbc_inference_plot_posterior --verbose\
-    --input-file  /home/lroy02/ground_based_acc/separate_runs/GW170729/demarg_nonzero_a_GW170729.hdf \
-    --output-file A3_separate_nonzero_a_GW170729_extrinsic.png \
+    --input-file /home/lroy02/ground_based_acc/project/workflow/test/workflow_outputs/local-site-scratch/work/H1L1V1-DEMARGINALIZATION_DEMARG_INFERENCE-GW170729_185629-0-10.hdf \
+    --output-file H1L1V1-PLOT_INFERENCE-GW170729_185629-0-10_extrinsic.png \
     --parameters \
         ra dec \
         inclination distance polarization coa_phase \
