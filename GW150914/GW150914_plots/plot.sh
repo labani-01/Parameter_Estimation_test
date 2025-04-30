c6ec8ac6403755aefd25c5215071e93bd431a52a
Labani Roy <labannyo2000@gmail.com>
2025-04-30 11:58:29 -0400

    GW150914_plots_after_correction_to_the_waveform_model

--- GW150914/GW150914_plots/plot.sh
+++ GW150914/GW150914_plots/plot.sh
@@ -1,6 +1,6 @@
 pycbc_inference_plot_posterior --verbose\
-    --input-file /home/lroy02/ground_based_acc/separate_runs/GW150914/demarg_nonzero_a_GW150914.hdf \
-    --output-file A1_separate_nonzero_a_GW150914_masses.png \
+    --input-file /home/lroy02/ground_based_acc/separate_runs/GW150914/demarg_nonzero_a_GW150914_new_model.hdf \
+    --output-file separate_nonzero_a_GW150914_masses.png \
     --parameters \
         'mass1_from_mchirp_q(mchirp, q):mass1' \
         'mass2_from_mchirp_q(mchirp, q):mass2' \
@@ -10,8 +10,8 @@ pycbc_inference_plot_posterior --verbose\
 
 
 pycbc_inference_plot_posterior --verbose\
-    --input-file /home/lroy02/ground_based_acc/separate_runs/GW150914/demarg_nonzero_a_GW150914.hdf \
-    --output-file A2_separate_nonzero_a_GW150914_spins.png \
+    --input-file /home/lroy02/ground_based_acc/separate_runs/GW150914/demarg_nonzero_a_GW150914_new_model.hdf \
+    --output-file separate_nonzero_a_GW150914_spins.png \
     --parameters \
         spin1_a spin2_a spin1_azimuthal spin2_azimuthal spin1_polar spin2_polar \
         'chi_eff_from_spherical(mass1_from_mchirp_q(mchirp, q), mass2_from_mchirp_q(mchirp, q), spin1_a, spin1_polar, spin2_a, spin2_polar):chi_eff' \
@@ -20,8 +20,8 @@ pycbc_inference_plot_posterior --verbose\
 
 
 pycbc_inference_plot_posterior --verbose\
-    --input-file /home/lroy02/ground_based_acc/separate_runs/GW150914/demarg_nonzero_a_GW150914.hdf \
-    --output-file A3_separate_nonzero_a_GW150914_extrinsic.png \
+    --input-file /home/lroy02/ground_based_acc/separate_runs/GW150914/demarg_nonzero_a_GW150914_new_model.hdf \
+    --output-file separate_nonzero_a_GW150914_extrinsic.png \
     --parameters \
         ra dec \
         inclination distance polarization coa_phase \
