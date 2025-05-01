3404bf1ae728ac961bacf7ec83e23a16b109d0a1
Labani Roy <labannyo2000@gmail.com>
2025-05-01 00:54:02 -0400

    added plots from 3 observations workflow runs

--- workflow/test_high_low_mass/plot.sh
+++ workflow/test_high_low_mass/plot.sh
@@ -1,10 +1,11 @@
 pycbc_inference_plot_posterior --verbose\
-    --input-file /home/lroy02/ground_based_acc/project/test_settings/inference_GW190521_gauss_emcee.hdf \
-    --output-file inference_GW190521_gauss_emcee.png \
+    --input-file /home/lroy02/ground_based_acc/project/workflow/test/workflow_outputs/local-site-scratch/work/H1L1V1-DEMARGINALIZATION_DEMARG_INFERENCE-GW170729_185629-0-10.hdf \
+    --output-file H1L1V1-DEMARGINALIZATION_DEMARG_INFERENCE-GW170729_185629-0-10.png \
     --parameters \
         'mass1_from_mchirp_q(mchirp, q):mass1' \
         'mass2_from_mchirp_q(mchirp, q):mass2' \
         mchirp q \
+        acc \
     --z-arg snr
 
 
