9fb0309cb887e6cbe65577ae3a38ae924b28fb5b
Labani Roy <labannyo2000@gmail.com>
2025-05-13 15:35:43 -0400

    plots GW200115

--- GW200115/GW200115_plots/plot.sh
+++ GW200115/GW200115_plots/plot.sh
@@ -1,16 +1,17 @@
 pycbc_inference_plot_posterior --verbose\
-    --input-file /home/lroy02/ground_based_acc/separate_runs/GW200115/demarg_GW200115_low_tc.hdf \
-    --output-file separate_GW200115_masses_low_dtc.png \
+    --input-file /home/lroy02/ground_based_acc/separate_runs/GW200115/demarg_nonzero_a_GW200115_low_tc.hdf \
+    --output-file separate_nonzero_a_GW200115_masses_low_dtc.png \
     --parameters \
         'mass1_from_mchirp_q(mchirp, q):mass1' \
         'mass2_from_mchirp_q(mchirp, q):mass2' \
         mchirp q \
+        acc \
     --z-arg snr
 
 
 pycbc_inference_plot_posterior --verbose\
-    --input-file /home/lroy02/ground_based_acc/separate_runs/GW200115/demarg_GW200115_low_tc.hdf \
-    --output-file separate_GW200115_spins_low_dtc.png \
+    --input-file /home/lroy02/ground_based_acc/separate_runs/GW200115/demarg_nonzero_a_GW200115_low_tc.hdf \
+    --output-file separate_nonzero_a_GW200115_spins_low_dtc.png \
     --parameters \
         spin1_a spin2_a spin1_azimuthal spin2_azimuthal spin1_polar spin2_polar \
         'chi_eff_from_spherical(mass1_from_mchirp_q(mchirp, q), mass2_from_mchirp_q(mchirp, q), spin1_a, spin1_polar, spin2_a, spin2_polar):chi_eff' \
@@ -19,8 +20,8 @@ pycbc_inference_plot_posterior --verbose\
 
 
 pycbc_inference_plot_posterior --verbose\
-    --input-file /home/lroy02/ground_based_acc/separate_runs/GW200115/demarg_GW200115_low_tc.hdf \
-    --output-file separate_GW200115_extrinsic_low_dtc.png \
+    --input-file /home/lroy02/ground_based_acc/separate_runs/GW200115/demarg_nonzero_a_GW200115_low_tc.hdf \
+    --output-file separate_nonzero_a_GW200115_extrinsic_low_dtc.png \
     --parameters \
         ra dec \
         inclination distance polarization coa_phase \
