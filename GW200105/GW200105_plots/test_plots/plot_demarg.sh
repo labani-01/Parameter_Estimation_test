15d5f93d0125240da645ba44334ff996d95632e3
Labani Roy <labannyo2000@gmail.com>
2025-06-17 12:06:19 -0400

    marginalized model and sampler setting and removed mass constraint

---
+++ GW200105/GW200105_plots/test_plots/plot_demarg.sh
@@ -0,0 +1,27 @@
+pycbc_inference_plot_posterior --verbose\
+    --input-file /home/lroy02/ground_based_acc/separate_runs/GW200105/test_runs/demarg_cal_lambda_frame_model_sampler_cons.hdf \
+    --output-file masses_cal_lambda_frame_model_sampler_cons.png \
+    --parameters \
+        'mass1_from_mchirp_q(mchirp, q):mass1' \
+        'mass2_from_mchirp_q(mchirp, q):mass2' \
+        mchirp q \
+    --z-arg snr
+
+
+pycbc_inference_plot_posterior --verbose\
+    --input-file /home/lroy02/ground_based_acc/separate_runs/GW200105/test_runs/demarg_cal_lambda_frame_model_sampler_cons.hdf \
+    --output-file spins_cal_lambda_frame_model_sampler_cons.png \
+    --parameters \
+        spin1_a spin2_a spin1_azimuthal spin2_azimuthal spin1_polar spin2_polar \
+        'chi_eff_from_spherical(mass1_from_mchirp_q(mchirp, q), mass2_from_mchirp_q(mchirp, q), spin1_a, spin1_polar, spin2_a, spin2_polar):chi_eff' \
+        'chi_p_from_spherical(mass1_from_mchirp_q(mchirp, q), mass2_from_mchirp_q(mchirp, q), spin1_a, spin1_azimuthal, spin1_polar, spin2_a, spin2_azimuthal, spin2_polar):chi_p' \
+    --z-arg snr
+
+
+pycbc_inference_plot_posterior --verbose\
+    --input-file /home/lroy02/ground_based_acc/separate_runs/GW200105/test_runs/demarg_cal_lambda_frame_model_sampler_cons.hdf \
+    --output-file extrinsic_cal_lambda_frame_model_sampler_cons.png \
+    --parameters \
+        ra dec \
+        inclination distance polarization coa_phase \
+    --z-arg snr
