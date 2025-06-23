7d8d52bc77ae9b7f202b271badc78fd45080c9bd
Labani Roy <labannyo2000@gmail.com>
2025-06-23 10:45:27 -0400

    nonzero acc with const lambda plots

--- GW190425/GW190425_plots/plot.sh
+++ GW190425/GW190425_plots/plot.sh
@@ -1,17 +1,17 @@
 pycbc_inference_plot_posterior --verbose\
-    --input-file /home/lroy02/ground_based_acc/separate_runs/GW190425/demarg_GW190425_zero_a.hdf \
-    --output-file separate_zero_a_GW190425_masses.png \
+    --input-file /home/lroy02/ground_based_acc/separate_runs/GW190425/demarg_GW190425_nonzero_a_const_lambda.hdf \
+    --output-file const_lambda_nonzero_a_GW190425_masses.png \
     --parameters \
         'mass1_from_mchirp_q(mchirp, q):mass1' \
         'mass2_from_mchirp_q(mchirp, q):mass2' \
         mchirp q \
-        lambda2 lambda1 \
+        acc \
     --z-arg snr
 
 
 pycbc_inference_plot_posterior --verbose\
-    --input-file /home/lroy02/ground_based_acc/separate_runs/GW190425/demarg_GW190425_zero_a.hdf \
-    --output-file separate_zero_a_GW190425_spins.png \
+    --input-file /home/lroy02/ground_based_acc/separate_runs/GW190425/demarg_GW190425_nonzero_a_const_lambda.hdf \
+    --output-file const_lambda_nonzero_a_GW190425_spins.png \
     --parameters \
         spin1_a spin2_a spin1_azimuthal spin2_azimuthal spin1_polar spin2_polar \
         'chi_eff_from_spherical(mass1_from_mchirp_q(mchirp, q), mass2_from_mchirp_q(mchirp, q), spin1_a, spin1_polar, spin2_a, spin2_polar):chi_eff' \
@@ -20,8 +20,8 @@ pycbc_inference_plot_posterior --verbose\
 
 
 pycbc_inference_plot_posterior --verbose\
-    --input-file /home/lroy02/ground_based_acc/separate_runs/GW190425/demarg_GW190425_zero_a.hdf \
-    --output-file separate_zero_a_GW190425_extrinsic.png \
+    --input-file /home/lroy02/ground_based_acc/separate_runs/GW190425/demarg_GW190425_nonzero_a_const_lambda.hdf \
+    --output-file const_lambda_nonzero_a_GW190425_extrinsic.png \
     --parameters \
         ra dec \
         inclination distance polarization coa_phase \
