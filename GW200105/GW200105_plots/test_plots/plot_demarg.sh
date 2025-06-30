a61b97a4184f18fde9fadf1bc32927a958e869db
Labani Roy <labannyo2000@gmail.com>
2025-06-30 13:53:40 -0400

    other plots

--- GW200105/GW200105_plots/test_plots/plot_demarg.sh
+++ GW200105/GW200105_plots/test_plots/plot_demarg.sh
@@ -1,6 +1,6 @@
 pycbc_inference_plot_posterior --verbose\
-    --input-file /home/lroy02/ground_based_acc/separate_runs/GW200105/test_runs/demarg_cal_lambda_frame_model_sampler_cons.hdf \
-    --output-file masses_cal_lambda_frame_model_sampler_cons.png \
+    --input-file /home/lroy02/ground_based_acc/separate_runs/GW200105/test_runs/demarg_cal_lambda_frame_model.hdf \
+    --output-file masses_cal_lambda_frame_model.png \
     --parameters \
         'mass1_from_mchirp_q(mchirp, q):mass1' \
         'mass2_from_mchirp_q(mchirp, q):mass2' \
@@ -9,8 +9,8 @@ pycbc_inference_plot_posterior --verbose\
 
 
 pycbc_inference_plot_posterior --verbose\
-    --input-file /home/lroy02/ground_based_acc/separate_runs/GW200105/test_runs/demarg_cal_lambda_frame_model_sampler_cons.hdf \
-    --output-file spins_cal_lambda_frame_model_sampler_cons.png \
+    --input-file /home/lroy02/ground_based_acc/separate_runs/GW200105/test_runs/demarg_cal_lambda_frame_model.hdf \
+    --output-file spins_cal_lambda_frame_model.png \
     --parameters \
         spin1_a spin2_a spin1_azimuthal spin2_azimuthal spin1_polar spin2_polar \
         'chi_eff_from_spherical(mass1_from_mchirp_q(mchirp, q), mass2_from_mchirp_q(mchirp, q), spin1_a, spin1_polar, spin2_a, spin2_polar):chi_eff' \
@@ -19,8 +19,8 @@ pycbc_inference_plot_posterior --verbose\
 
 
 pycbc_inference_plot_posterior --verbose\
-    --input-file /home/lroy02/ground_based_acc/separate_runs/GW200105/test_runs/demarg_cal_lambda_frame_model_sampler_cons.hdf \
-    --output-file extrinsic_cal_lambda_frame_model_sampler_cons.png \
+    --input-file /home/lroy02/ground_based_acc/separate_runs/GW200105/test_runs/demarg_cal_lambda_frame_model.hdf \
+    --output-file extrinsic_cal_lambda_frame_model.png \
     --parameters \
         ra dec \
         inclination distance polarization coa_phase \
---
+++ GW200105/GW200105_plots/test_plots/plot_small.sh
@@ -0,0 +1,8 @@
+pycbc_inference_plot_posterior --verbose\
+    --input-file /home/lroy02/ground_based_acc/separate_runs/GW200105/test_runs/4_ogc_cal_lambda_frame_modelsetting1_const_inference5.hdf \
+    --output-file masses_4_ogc_cal_lambda_frame_modelsetting1_const_inference5.png \
+    --parameters \
+        'mass1_from_mchirp_q(mchirp, q):mass1' \
+        'mass2_from_mchirp_q(mchirp, q):mass2' \
+        mchirp q \
+    --z-arg snr
