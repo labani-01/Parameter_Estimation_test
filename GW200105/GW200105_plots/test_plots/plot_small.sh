bfe4c4f7709b2ee511dda10b8c81a2680d3aa2ac
Labani Roy <labannyo2000@gmail.com>
2025-07-02 12:05:20 -0400

    new plot

--- GW200105/GW200105_plots/test_plots/plot_small.sh
+++ GW200105/GW200105_plots/test_plots/plot_small.sh
@@ -1,6 +1,6 @@
 pycbc_inference_plot_posterior --verbose\
-    --input-file /home/lroy02/ground_based_acc/separate_runs/GW200105/test_runs/4_ogc_cal_lambda_frame_modelsetting1_const_inference5.hdf \
-    --output-file masses_4_ogc_cal_lambda_frame_modelsetting1_const_inference5.png \
+    --input-file /home/lroy02/ground_based_acc/separate_runs/GW200105/test_runs/4_ogc_cal_lambda_frame_modelsetting1_const_inference7.hdf \
+    --output-file masses_4_ogc_cal_lambda_frame_modelsetting1_const_inference7.png \
     --parameters \
         'mass1_from_mchirp_q(mchirp, q):mass1' \
         'mass2_from_mchirp_q(mchirp, q):mass2' \
